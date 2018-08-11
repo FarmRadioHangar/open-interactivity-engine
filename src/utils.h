bfaa5742a62701443f6425546397dc3076f54f1d
Johannes Hilden <hildenjohannes@gmail.com>
2018-08-11 22:15:40 +0300

    add utils namespace

---
+++ src/utils.h
@@ -0,0 +1,23 @@
+/*!
+ * \file utils.h
+ */
+namespace polls
+{
+    namespace utils
+    {
+        namespace builder
+        {
+            class document
+            {
+            };
+
+            class array
+            {
+            };
+
+            class response
+            {
+            };
+        }
+    }
+}
