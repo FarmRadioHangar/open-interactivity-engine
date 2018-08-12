c373cedd3637cd15829a3af5d811812672d7c312
Johannes Hilden <hildenjohannes@gmail.com>
2018-08-12 08:29:37 +0300

    cast name to string

--- client/src/components/campaignsList.js
+++ client/src/components/campaignsList.js
@@ -4,18 +4,20 @@ import { Link } from 'react-router-dom'
 
 const CampaignsList = ({campaigns}) => {
   return (
-    <ul className='list-group'>
-      {campaigns.map(campaign => {
-        const id = campaign['_id']['$oid'];
-        return (
-          <li className='list-group-item' key={id}>
-            <span>{campaign.name}</span>
-            <Link to={`/campaigns/${id}/edit`}>Edit</Link>
-            <Link to={`/campaigns/${id}/delete`}>Delete</Link>
-          </li>
-        );
-      })}
-    </ul>
+    <div>
+      <ul className='list-group'>
+        {campaigns.map(campaign => {
+          const id = campaign['_id']['$oid'];
+          return (
+            <li className='list-group-item' key={id}>
+              <span>{String(campaign.name)}</span>
+              <Link to={`/campaigns/${id}/edit`}>Edit</Link>
+              <Link to={`/campaigns/${id}/delete`}>Delete</Link>
+            </li>
+          );
+        })}
+      </ul>
+    </div>
   );
 };
 
