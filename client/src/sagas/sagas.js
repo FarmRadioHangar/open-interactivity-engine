4373d72cd98c03598e07f1b12c53eab050c2347e
Johannes Hilden <hildenjohannes@gmail.com>
2018-08-19 02:21:32 +0300

    add sagas

---
+++ client/src/sagas/sagas.js
@@ -0,0 +1,27 @@
+import { fork, all, call, put, takeEvery } from 'redux-saga/effects';
+import * as types from '../actions/actionTypes';  
+import api from '../api';
+
+function* callGetLanguagesSaga(action) {
+  try {
+    const response = yield call(api.get, 'languages', {});
+    yield put({ 
+      type: types.FETCH_LANGUAGES_DONE, 
+      items: response.languages,
+      count: response.count,
+      total: response.total
+    });
+  } catch(err) {
+    yield put({ type: types.FETCH_LANGUAGES_FAILED });
+  }
+}
+
+function* getLanguagesSaga() {
+  yield takeEvery(types.FETCH_LANGUAGES, callGetLanguagesSaga);
+}
+
+export function* rootSaga() {
+  yield all([
+    fork(getLanguagesSaga)
+  ]);
+}
