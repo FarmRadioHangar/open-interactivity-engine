8f7a1cbd4af9d112269ba5690bafdb8cada17954
Johannes Hilden <hildenjohannes@gmail.com>
2018-08-19 22:41:37 +0300

    implement lazy list helper

--- client/src/components/languagesIndex.js
+++ client/src/components/languagesIndex.js
@@ -1,24 +1,35 @@
-import React from 'react';  
+import React from 'react';
 import PropTypes from 'prop-types'
-import { connect } from 'react-redux';  
+import { connect } from 'react-redux';
 import { Link } from 'react-router-dom'
 import * as languagesActions from '../actions/languagesActions';
 import LanguagesList from './languagesList';
+import history from '../history';
+import Pagination from './pagination';
 
-class LanguagesIndex extends React.Component {  
+class LanguagesIndex extends React.Component {
   componentDidMount() {
     this.props.dispatch(languagesActions.fetchLanguages());
   }
   render() {
+    const { languages, dispatch } = this.props;
     return(
       <span>
-        <LanguagesList 
-          languages={this.props.languages} 
-          onReload={() => {
-            this.props.dispatch(languagesActions.fetchLanguages());
+        <LanguagesList
+          languages = {languages}
+          onReload  = {() => {
+            dispatch(languagesActions.fetchLanguages());
           }}
         />
         <Link to='/languages/create'>Add language</Link>
+        <Pagination
+          pageSize = {languages.pageSize}
+          total    = {languages.total}
+          offset   = {languages.offset}
+          onChange = {pageNumber => {
+            history.push(`/languages/page/${pageNumber}`);
+          }}
+        />
       </span>
     );
   }
@@ -32,6 +43,6 @@ function mapStateToProps(state, ownProps) {
   return {
     languages: state.languages
   };
-} 
+}
 
-export default connect(mapStateToProps)(LanguagesIndex);  
+export default connect(mapStateToProps)(LanguagesIndex);
---
+++ client/src/components/pagination.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import List from '../list';
+
+const Pagination = ({ pageSize, total, offset, onChange }) => {
+  const pageCount = Math.ceil(total/pageSize);
+  const currentPage = offset/pageSize + 1;
+  return (
+    <ul>
+      {List.enumFrom(1).take(pageCount).toArray().map(page =>
+        <li key={page}>
+          {page == currentPage ? (
+            <span>{page}</span>
+          ) : (
+            <a href='#' onClick={() => onChange(page)}>
+              {page}
+            </a>
+          )}
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Pagination;
