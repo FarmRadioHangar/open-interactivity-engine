25dcf3f830231cbe1ef8558eaba988f4d40ce260
Johannes Hilden <hildenjohannes@gmail.com>
2018-08-22 22:55:37 +0300

    update builder

--- src/utils.h
+++ src/utils.h
@@ -12,50 +12,53 @@ namespace polls
     {
         namespace builder
         {
-            struct property
-            {
-                std::string                  name;
-                web::json::value::value_type type;
-            };
-
             template <typename T> class document
             {
             public:
                 document(const web::json::value& json);
                 document(const std::string& data);
+
                 ~document() = default;
                 document(const document&) = delete;
                 document& operator=(const document&) = delete;
 
-                void add_required_property(const std::string& name, web::json::value::value_type type);
+                void add_required_property(const std::string& name,
+                    web::json::value::value_type type);
+
                 T build();
 
             protected:
                 bsoncxx::types::value to_bson_value(const web::json::value& json);
 
             private:
-                web::json::value      _json;
-                std::vector<property> _properties;   // \todo use a map instead of vector?
+                using prop_map = std::map<std::string, web::json::value::value_type>;
+
+                web::json::value _json;
+                prop_map         _properties;
             };
 
-            template <typename T> document<T>::document(const web::json::value& json)
+            template <typename T>
+            document<T>::document(const web::json::value& json)
               : _json{json}
             {
             }
 
-            template <typename T> document<T>::document(const std::string& data)
+            template <typename T>
+            document<T>::document(const std::string& data)
               : _json{web::json::value::parse(data)}
             {
             }
 
-            template <typename T> void document<T>::add_required_property(
+            template <typename T>
+            void document<T>::add_required_property(
                 const std::string& name,
                 web::json::value::value_type type)
             {
-                _properties.push_back({name, type});
+                _properties.insert({name, type});
             }
 
-            template <typename T> T document<T>::build()
+            template <typename T>
+            T document<T>::build()
             {
                 bsoncxx::builder::basic::document builder{};
                 T document;
@@ -64,24 +67,24 @@ namespace polls
                     throw std::runtime_error{"json data must be an object"};
                 }
 
-                for (auto& property : _properties) {
-                    if (!_json.has_field(property.name)) {
+                for (auto& prop : _properties) {
+                    if (!_json.has_field(prop.first)) {
                         std::stringstream oss{};
-                        oss << "missing property: " << property.name;
+                        oss << "missing property: " << prop.first;
                         throw std::runtime_error{oss.str()};
                     }
-                    auto val = _json[property.name];
-                    if (val.type() != property.type) {
+                    auto val = _json[prop.first];
+                    if (val.type() != prop.second) {
                         throw std::runtime_error{"type mismatch"};
                     }
-                    builder.append(kvp(property.name, to_bson_value(val)));
+                    builder.append(kvp(prop.first, to_bson_value(prop.second)));
                 }
 
                 document.set_data(bsoncxx::to_json(builder.extract()));
                 return document;
             }
 
-            template <typename T> bsoncxx::types::value 
+            template <typename T> bsoncxx::types::value
             document<T>::to_bson_value(const web::json::value& json)
             {
                 using namespace bsoncxx::types;
@@ -102,19 +105,19 @@ namespace polls
                         return value{b_utf8{json.as_string()}};
                     case web::json::value::value_type::Object:
                     {
-                        bsoncxx::builder::basic::document val{};
+                        bsoncxx::builder::basic::document builder{};
                         for (auto& doc : json.as_object()) {
-                            val.append(kvp(doc.first, to_bson_value(doc.second)));
+                            builder.append(kvp(doc.first, to_bson_value(doc.second)));
                         }
-                        return value{b_document{val.extract()}};
+                        return value{b_document{builder.extract()}};
                     }
                     case web::json::value::value_type::Array:
                     {
-                        bsoncxx::builder::basic::array docs{};
+                        bsoncxx::builder::basic::array builder{};
                         for (auto& doc : json.as_array()) {
-                            docs.append(to_bson_value(doc));
+                            builder.append(to_bson_value(doc));
                         }
-                        return value{b_array{docs.extract()}};
+                        return value{b_array{builder.extract()}};
                     }
                     case web::json::value::value_type::Null:
                     default:
