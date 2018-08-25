5250cda98cfee41de4a9cd54d5ffa7a98d108d53
Johannes Hilden <hildenjohannes@gmail.com>
2018-08-25 13:37:57 +0300

    add pagination component

---
+++ client/src/components/pagination.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import List from '../list';
+
+const Pagination = ({ pageSize, total, offset, onChange }) => {
+  const pageCount = Math.ceil(total/pageSize);
+  const currentPage = offset/pageSize + 1;
+  if (pageCount < 2) { return <React.Fragment /> }
+  return (
+    <ul>
+      {List.enumFrom(1).take(pageCount).toArray().map(page =>
+        <li key={page}>
+          {page == currentPage ? (
+            <span>{page}</span>
+          ) : (
+            <a href='#' onClick={e => { e.preventDefault(); onChange(page) }}>
+              {page}
+            </a>
+          )}
+        </li>
+      )}
+    </ul>
+  );
+};
+
+export default Pagination;
