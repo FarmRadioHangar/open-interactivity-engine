da60ff84197a92b992ff7f67c167375661182316
Johannes Hilden <hildenjohannes@gmail.com>
2018-08-27 13:24:33 +0300

    add missing semi-colons

--- client/src/components/pagination.js
+++ client/src/components/pagination.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import PropTypes from 'prop-types'
+import PropTypes from 'prop-types';
 import List from '../utils/list';
 
 const Pagination = ({ pageCount, currentPage, onChange }) => {
