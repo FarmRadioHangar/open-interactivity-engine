078e59726e942c28e31b154f6cc7078dd8e08b37
Johannes Hilden <hildenjohannes@gmail.com>
2018-09-07 22:23:12 +0300

    implement more tests

--- src/model.h
+++ src/model.h
@@ -5,7 +5,9 @@
 
 #include <bsoncxx/builder/basic/kvp.hpp>
 #include <bsoncxx/exception/exception.hpp>
+#include <bsoncxx/json.hpp>
 #include <bsoncxx/oid.hpp>
+#include <cpprest/json.h>
 #include <mongocxx/client.hpp>
 #include <vector>
 #include "model/exception.h"
@@ -23,7 +25,75 @@ namespace survey
      */
     template <typename T, typename Collection = std::vector<T>> class page
     {
+    public:
+        page(Collection&& container, const std::size_t total);
+
+        std::size_t count() const;
+        std::size_t total() const;
+
+        web::json::value to_json() const;
+
+    private:
+        Collection  _collection;
+        std::size_t _total;
     };
+
+    /*!
+     * \brief Default constructor
+     */
+    template <typename T, typename Collection>
+    page<T, Collection>::page(Collection&& collection, const std::size_t total)
+      : _collection{std::move(collection)},
+        _total{total}
+    {
+        assert(total >= collection.size());
+    }
+
+    /*!
+     * \brief Return the number of documents in this collection. This number
+     *        is often less than the number of documents available in the
+     *        underlying MongoDB data store.
+     *
+     * \sa total
+     */
+    template <typename T, typename Collection>
+    std::size_t page<T, Collection>::count() const
+    {
+        return _collection.size();
+    }
+
+    /*!
+     * \brief The total number of documents in the MongoDB collection from
+     *        where this subset originates.
+     *
+     * \returns the \a total number of documents available in the MongoDB
+     *          collection accommodating this subset, at the time it was
+     *          generated.
+     */
+    template <typename T, typename Collection>
+    std::size_t page<T, Collection>::total() const
+    {
+        return _total;
+    }
+
+    /*!
+     * \brief Create and return a JSON object representation of the collection.
+     *
+     * \returns a json object holding an array of documents
+     */
+    template <typename T, typename Collection>
+    web::json::value page<T, Collection>::to_json() const
+    {
+        std::vector<web::json::value> values;
+        for (const auto& value : _collection) {
+            values.emplace_back(web::json::value::parse(value.data()));
+        }
+        web::json::value obj{};
+        obj[T::mongodb_collection] = web::json::value::array(values);
+        obj["count"] = count();
+        obj["total"] = total();
+        return obj;
+    }
 
     /*!
      * \class model
@@ -55,6 +125,8 @@ namespace survey
     template <typename T> class model
     {
     public:
+        static constexpr std::int64_t default_page_limit = 60;
+
         //model();
         model(const std::string& db, const std::string& collection = T::mongodb_collection);
         model(const model& other);
@@ -64,12 +136,33 @@ namespace survey
         std::string db() const;
         std::string collection() const;
 
+        void set_data(web::json::value&& data);
+        void set_data(const web::json::value& data);
+        web::json::value data() const;
+
         void set_oid(const std::string& oid);
         std::string oid() const;
+
+        void fetch();
+        void save();
+        void remove();
+
+        static T get(const std::string& oid);
+
+        static std::int64_t count();
+
+        template <template <typename, typename> class Container = std::vector,
+                  template <typename> class Allocator = std::allocator>
+        static survey::page<T, Container<T, Allocator<T>>>
+        page(std::int64_t skip = 0, std::int64_t limit = default_page_limit);
+
+    protected:
+        mongocxx::collection get_mongodb_collection() const;
 
     private:
         std::string      _db;
         std::string      _collection;
+        web::json::value _data;
         bsoncxx::oid     _oid;
         mongocxx::client _client;
     };
@@ -77,7 +170,7 @@ namespace survey
 //    /*!
 //     * \brief Default constructor
 //     */
-//    template <typename T> model<T>::model() 
+//    template <typename T> model<T>::model()
 //      : _client{mongocxx::uri{}}
 //    {
 //    }
@@ -92,6 +185,7 @@ namespace survey
     model<T>::model(const std::string& db, const std::string& collection)
       : _db{db},
         _collection{collection},
+        _data{web::json::value::object()},
         _client{mongocxx::uri{}}
     {
     }
@@ -102,6 +196,7 @@ namespace survey
     template <typename T> model<T>::model(const model& other)
       : _db{other._db},
         _collection{other._collection},
+        _data{other._data},
         _oid{other._oid},
         _client{mongocxx::uri{}}
     {
@@ -116,15 +211,14 @@ namespace survey
         {
             _db         = other._db;
             _collection = other._collection;
+            _data       = other._data;
             _oid        = other._oid;
         }
         return *this;
     }
 
     /*!
-     * \brief todo
-     *
-     * \return todo
+     * \return the name of the MongoDB database this document is linked to
      */
     template <typename T> std::string model<T>::db() const
     {
@@ -132,13 +226,49 @@ namespace survey
     }
 
     /*!
-     * \brief todo
-     *
-     * \return todo
+     * \return the name of the MongoDB collection this document is linked to
      */
     template <typename T> std::string model<T>::collection() const
     {
         return _collection;
+    }
+
+    /*!
+     * \brief Transfer ownership of the provided JSON value to the document.
+     *
+     * \param data - a JSON object
+     */
+    template <typename T> void model<T>::set_data(web::json::value&& data)
+    {
+        if (!data.is_object()) {
+            throw model_error{model_error::bad_bson_data, "JSON value must be an object"};
+        }
+
+        _data = std::move(data);
+    }
+
+    /*!
+     * \brief Set the document's data.
+     *
+     * \param data - a JSON object
+     */
+    template <typename T> void model<T>::set_data(const web::json::value& data)
+    {
+        if (!data.is_object()) {
+            throw model_error{model_error::bad_bson_data, "JSON value must be an object"};
+        }
+
+        _data = data;
+    }
+
+    /*!
+     * \brief Get the document's data.
+     *
+     * \return todo
+     */
+    template <typename T> web::json::value model<T>::data() const
+    {
+        return _data;
     }
 
     /*!
@@ -163,6 +293,133 @@ namespace survey
     template <typename T> std::string model<T>::oid() const
     {
         return _oid.to_string();
+    }
+
+    /*!
+     * \brief Fetch the document's data from the database.
+     */
+    template <typename T> void model<T>::fetch()
+    {
+        auto filter = bsoncxx::builder::basic::make_document(kvp("_id", _oid));
+        auto result = get_mongodb_collection().find_one(filter.view());
+
+        if (!result) {
+            throw model_error{model_error::document_not_found, "not found"};
+        }
+
+        _data = web::json::value::parse(bsoncxx::to_json(result.value()));
+    }
+
+    /*!
+     * \brief Persist the document to the database. This method can be used
+     *        either to update an existing document; or to create a new one.
+     */
+    template <typename T> void model<T>::save()
+    {
+        std::unique_ptr<bsoncxx::document::value> data;
+
+        if (_data.is_null()) {
+            throw model_error{model_error::empty_document, "empty document"};
+        }
+
+        try {
+            data = std::make_unique<bsoncxx::document::value>(bsoncxx::from_json(_data.serialize()));
+        } catch (bsoncxx::v_noabi::exception&) {
+            throw model_error{model_error::bad_bson_data, "bad BSON data"};
+        }
+
+        auto filter = bsoncxx::builder::basic::make_document(kvp("_id", _oid));
+
+        mongocxx::options::update options{};
+        options.upsert(true);
+
+        get_mongodb_collection().replace_one(filter.view(), data->view(), options);
+
+        fetch();
+    }
+
+    /*!
+     * \brief Delete the document from the database. This leaves the object in
+     *        a fresh (newly constructed) state.
+     */
+    template <typename T> void model<T>::remove()
+    {
+        auto filter = bsoncxx::builder::basic::make_document(kvp("_id", _oid));
+
+        get_mongodb_collection().delete_one(filter.view());
+
+        _oid  = bsoncxx::oid{};
+        _data = web::json::value{};
+    }
+
+    /*!
+     * \brief Get the MongoDB document corresponding to the provided ObjectId.
+     *
+     * \param id - a valid MongoDB ObjectId string
+     *
+     * \return a document
+     */
+    template <typename T> T model<T>::get(const std::string& oid)
+    {
+        T document{};
+
+        document.set_oid(oid);
+        document.fetch();
+
+        return document;
+    }
+
+    /*!
+     * \brief Get a subset of documents from a MongoDB collection.
+     *
+     * \param skip  - offset from where MongoDB begins returning results
+     * \param limit - the maximum number of documents to return
+     *
+     * \return a STL container with a collection of documents
+     */
+    template <typename T>
+    template <template <typename, typename> class Container,
+              template <typename> class Allocator>
+    survey::page<T, Container<T, Allocator<T>>>
+    model<T>::page(std::int64_t skip, std::int64_t limit)
+    {
+        T model{};
+
+        mongocxx::options::find opts{};
+        opts.skip(skip);
+        opts.limit(limit);
+
+        auto collection = model.collection();
+        auto cursor = collection.find({}, opts);
+
+        Container<T, Allocator<T>> container{};
+
+        for (auto&& doc : cursor)
+        {
+            T document{};
+            document._oid  = doc["_id"].get_oid().value;
+            document._data = bsoncxx::to_json(doc);
+            container.push_back(document);
+        }
+
+        return survey::page<T, Container<T, Allocator<T>>>{
+            std::move(container),
+            static_cast<std::size_t>(collection.count({}))
+        };
+    }
+
+    /*!
+     * \brief Return the \a total number of documents available in the MongoDB
+     *        collection to which this model is linked.
+     */
+    template <typename T> std::int64_t model<T>::count()
+    {
+        return T{}.collection().count({});
+    }
+
+    template <typename T> mongocxx::collection model<T>::get_mongodb_collection() const
+    {
+        return _client.database(_db).collection(_collection);
     }
 }
 
@@ -234,7 +491,7 @@ namespace survey
 //    }
 //
 //    /*!
-//     * \brief The total number of documents in the MongoDB collection from 
+//     * \brief The total number of documents in the MongoDB collection from
 //     * where this subset originates.
 //     *
 //     * \returns the \a total number of documents available in the MongoDB
--- test/main.cpp
+++ test/main.cpp
@@ -1,7 +1,8 @@
+#include "gtest/gtest.h"
 #include "../src/model.h"
 #include "../src/model/exception.h"
-#include "gtest/gtest.h"
 #include <mongocxx/instance.hpp>
+#include <cpprest/json.h>
 
 class pants : public survey::model<pants>
 {
@@ -11,22 +12,22 @@ public:
     pants() : survey::model<pants>{"test"} {}
 };
 
-TEST(model_test, document_initialization) 
-{ 
+TEST(model_test, document_initialization)
+{
     pants document;
 
     ASSERT_EQ("test", document.db());
     ASSERT_EQ("pants", document.collection());
 }
 
-TEST(model_test, oid_is_not_empty) 
+TEST(model_test, oid_is_not_empty)
 {
     pants document;
 
     ASSERT_FALSE(document.oid().empty());
 }
 
-TEST(model_test, copy_matches_original) 
+TEST(model_test, copy_props_matches_original)
 {
     pants document;
     pants other(document);
@@ -37,7 +38,7 @@ TEST(model_test, copy_matches_original)
     ASSERT_EQ("pants", other.collection());
 }
 
-TEST(model_test, copy_assign_matches_original) 
+TEST(model_test, copy_assign_props_matches_original)
 {
     pants document;
     pants other;
@@ -50,7 +51,29 @@ TEST(model_test, copy_assign_matches_original)
     ASSERT_EQ("pants", other.collection());
 }
 
-TEST(model_test, set_bad_oid_throws) 
+TEST(model_test, copy_data_matches_original)
+{
+    auto data = web::json::value::parse("{\"some\":\"string\"}");
+
+    {
+        pants document;
+        document.set_data(data);
+
+        pants other(document);
+
+        ASSERT_EQ(document.data(), other.data());
+    }
+    {
+        pants document;
+        pants other;
+
+        other = document;
+
+        ASSERT_EQ(document.data(), other.data());
+    }
+}
+
+TEST(model_test, set_bad_oid_throws)
 {
     pants document;
 
@@ -61,6 +84,75 @@ TEST(model_test, set_bad_oid_throws)
     } catch(const survey::model_error& e) {
         ASSERT_EQ(survey::model_error::bad_oid, e.type());
     }
+}
+
+TEST(model_test, set_oid)
+{
+    pants document{};
+    bsoncxx::oid oid{};
+
+    const std::string valid_oid(oid.to_string());
+
+    document.set_oid(valid_oid);
+
+    ASSERT_EQ(valid_oid, document.oid());
+}
+
+TEST(model_test, save_and_get_document)
+{
+    pants document{};
+
+    auto data = web::json::value::parse("{\"this\":\"prop\"}");
+    document.set_data(data);
+    document.save();
+
+    const std::string oid = document.oid();
+
+    pants other = pants::get(oid);
+
+    ASSERT_EQ(oid, other.oid());
+    ASSERT_TRUE(other.data()["this"].is_string());
+    ASSERT_EQ("prop", other.data()["this"].as_string());
+}
+
+TEST(model_test, set_non_object_throws)
+{
+    pants document;
+    const auto json_array = web::json::value::parse("[1,2,3]");
+
+    ASSERT_THROW(document.set_data(json_array), survey::model_error);
+
+    try {
+        document.set_data(json_array);
+    } catch(const survey::model_error& e) {
+        ASSERT_EQ(survey::model_error::bad_bson_data, e.type());
+    }
+
+    ASSERT_THROW(document.set_data(std::move(json_array)), survey::model_error);
+}
+
+TEST(model_test, get_removed_document_throws)
+{
+    pants document;
+
+    document.save();
+
+    const auto oid = document.oid();
+
+    document.remove();
+
+    ASSERT_THROW(pants::get(oid), survey::model_error);
+}
+
+TEST(model_test, removed_document_is_empty)
+{
+    pants document;
+    document.set_data(web::json::value::parse("{\"some\":\"string\"}"));
+
+    document.save();
+    document.remove();
+
+    ASSERT_EQ(document.data(), web::json::value{});
 }
 
 int main(int argc, char* argv[])
