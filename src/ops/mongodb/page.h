8b8db912befeffbd533b705b084caaa6ae3cf7fe
Johannes Hildén <hildenjohannes@gmail.com>
2018-10-16 10:21:10 +0200

    commit page

---
+++ src/ops/mongodb/page.h
@@ -0,0 +1,18 @@
+///
+/// \file page.h
+///
+#pragma once
+
+namespace ops
+{
+    namespace mongodb
+    {
+        ///
+        /// A \a page is a subset of documents drawn from a MongoDB collection.
+        ///
+        template <typename T>
+        class page
+        {
+        };
+    }
+}
