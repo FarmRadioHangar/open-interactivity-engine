04e698fa4a6feb9aaf09c3edee475d56be85eded
Johannes Hildén <hildenjohannes@gmail.com>
2018-10-23 13:53:33 +0300

    use unique ptr to create controllers

--- src/main.cpp
+++ src/main.cpp
@@ -15,32 +15,32 @@ int main()
 
     ops::http::rest::server server;
 
-    ops::campaigns_controller campaigns;
-    ops::languages_controller languages;
-    ops::content_controller content;
+    auto campaigns = std::make_unique<ops::campaigns_controller>();
+    auto languages = std::make_unique<ops::languages_controller>();
+    auto content = std::make_unique<ops::content_controller>();
 
-    server.add_controller("campaigns", &campaigns);
+    server.add_controller("campaigns", campaigns.get());
 
     server.add_route(methods::POST, "^/campaigns/([0-9a-f]+)/features$",
-        campaigns.bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::post_feature));
+        campaigns->bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::post_feature));
 
     server.add_route(methods::PATCH, "^/campaigns/([0-9a-f]+)/features/([0-9a-f]+)$",
-        campaigns.bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::patch_feature));
+        campaigns->bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::patch_feature));
 
     server.add_route(methods::POST, "^/campaigns/([0-9a-f]+)/features/([0-9a-f]+)/adapters$",
-        campaigns.bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::post_adapter));
+        campaigns->bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::post_adapter));
 
     server.add_route(methods::POST, "^/campaigns/([0-9a-f]+)/languages$",
-        campaigns.bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::post_language));
+        campaigns->bind_handler<ops::campaigns_controller>(&ops::campaigns_controller::post_language));
 
-    server.add_controller("languages", &languages);
-    server.add_controller("content", &content);
+    server.add_controller("languages", languages.get());
+    server.add_controller("content", content.get());
 
     server.add_route(methods::POST, "^/content/([0-9a-f]+)/reps$",
-        content.bind_handler<ops::content_controller>(&ops::content_controller::post_rep));
+        content->bind_handler<ops::content_controller>(&ops::content_controller::post_rep));
 
     server.add_route(methods::POST, "^/media$",
-        content.bind_handler<ops::content_controller>(&ops::content_controller::post_media));
+        content->bind_handler<ops::content_controller>(&ops::content_controller::post_media));
 
     server.run();
 
