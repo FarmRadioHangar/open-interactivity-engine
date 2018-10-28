e9e7f404e1c021510889e80fe03da81246946a04
Johannes Hildén <hildenjohannes@gmail.com>
2018-10-28 12:53:34 +0300

    add audience and country controllers

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
-#include "ops/http/rest/server.h"
-#include "ops/mongodb/pool.h"
+#include "core/controllers/audience.h"
 #include "core/controllers/campaigns.h"
 #include "core/controllers/content.h"
+#include "core/controllers/countries.h"
 #include "core/controllers/languages.h"
 #include "core/controllers/media.h"
 #include "dotenv/dotenv.h"
 #include "nexmo/adapters/nexmo_voice.h"
+#include "ops/http/rest/server.h"
+#include "ops/mongodb/pool.h"
 
 int main()
 {
@@ -21,15 +23,15 @@ int main()
     auto languages = std::make_unique<core::languages_controller>();
     auto content = std::make_unique<core::content_controller>();
     auto media = std::make_unique<core::media_controller>();
-//    auto audience = std::make_unique<core::audience_controller>();
-//    auto country = std::make_unique<core::country_controller>();
+    auto audience = std::make_unique<core::audience_controller>();
+    auto country = std::make_unique<core::countries_controller>();
 
     server.add_controller("campaigns", campaigns.get());
     server.add_controller("languages", languages.get());
     server.add_controller("content", content.get());
     server.add_controller("media", media.get());
-//    server.add_controller("audience", audience.get());
-//    server.add_controller("country", country.get());
+    server.add_controller("audience", audience.get());
+    server.add_controller("country", country.get());
 
     //
 
